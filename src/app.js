2a96dd81bc7350b1d60f7eedacb7e5506e4e03bb
Mark Romero <markromero@marks-mbp.lan>
2018-12-13 00:41:18 -0700

    added index css and js files

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const App = () => {
+    return React.createElement("div", {},
+       React.createElement("h1", {}, "Pet Adoption")
+    )
+}
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
